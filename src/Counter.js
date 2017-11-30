2710b7373cfc330c95180a09d463da3000fbbf65
Prateek Thapa <prateekthapa26@gmail.com>
2017-11-30 22:18:22 +0545

    Container Pattern

--- src/Counter.js
+++ src/Counter.js
@@ -1,55 +1,23 @@
-import React, { Component } from 'react';
-import './style.css';
+import React from 'react';
 
-class Counter extends Component {
-  state = {
-    count: 0
-  };
-
-  //callback to the setState
-  incrementCount = () => {
-    this.setState(
-      state => {
-        return { count: state.count + 1 };
-      },
-      () => {
-        if (this.state.count > 5) {
-          this.resetCount();
-        }
-      }
-    );
-  };
-
-  //setting state by a function
-  decrementCount = () => {
-    this.setState(state => {
-      return { count: state.count - 1 };
-    });
-  };
-
-  //normal setState
-  resetCount = () => {
-    this.setState({ count: 0 });
-  };
-
-  render() {
-    return (
-      <main className="counterContainer">
-        <section className="Counter">
-          <h1>Count: {this.state.count}</h1>
-          <button onClick={this.incrementCount} className="full-width">
-            Increment
-          </button>
-          <button onClick={this.decrementCount} className="full-width">
-            Decrement
-          </button>
-          <button onClick={this.resetCount} className="full-width">
-            Reset
-          </button>
-        </section>
-      </main>
-    );
-  }
-}
+const Counter = props => {
+  const { count, incrementCount, decrementCount, resetCount } = props;
+  return (
+    <main className="counterContainer">
+      <section className="Counter">
+        <h1>Count: {count}</h1>
+        <button onClick={incrementCount} className="full-width">
+          Increment
+        </button>
+        <button onClick={decrementCount} className="full-width">
+          Decrement
+        </button>
+        <button onClick={resetCount} className="full-width">
+          Reset
+        </button>
+      </section>
+    </main>
+  );
+};
 
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Counter from './Counter';
+import CounterContainer from './CounterContainer';
 
-render(<Counter />, document.getElementById('root'));
+render(<CounterContainer />, document.getElementById('root'));
